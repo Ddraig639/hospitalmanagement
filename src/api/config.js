2dad374a940fbd35c3d2a166b49605b41affc1cd
wisdom <carowhite639@gmail.com]>
2025-12-01 08:49:37 +0100

    cc

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 // src/api/config.js
 
-export const API_BASE_URL = "http://localhost:8000"; // FastAPI backend URL
+export const API_BASE_URL = "https://hospitalbackend-7u4y.onrender.com"; // FastAPI backend URL
 
 export const getAuthHeaders = () => {
   const token = sessionStorage.getItem("token");
